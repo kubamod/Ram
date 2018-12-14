ab68fe70010f81d183dbb6648f229376ab7e8e05
Kuba Modrzejewski <kubam1414@hotmail.com>
2018-12-14 10:36:27 +0100

    text added

--- template-parts/mainPage.php
+++ template-parts/mainPage.php
@@ -13,8 +13,10 @@
 			<div class="col-sm">
 				<div class="my-auto">
 				<h2>Gwarancja bezpieczeństwa</h2>
-				Wybierając nasze usługi zapewniasz sobie gwarancję dobrego wykonania i bezpieczeństwa sieci.
-				
+			Świadczymy usługi najwyższej jakości o najwyższym poziomie bezpieczeństwa. 
+Już nie będziesz musiał martwić się o swoją sieć komputerową w firmie. 
+W naszych rękach jest bezpieczna. Wystarczy, że do nas zadzwonisz.  
+
 				</div>
 			</div>
 		</div>	
@@ -29,7 +31,10 @@
 				<img src="<?php echo bloginfo('template_directory'); ?>/assets/images/5.png" class="img-fluid" width="900" />	
 			</div>
 			<div class="col-sm">
-				<div class="my-auto"><h2>Zakładamy sieci</h2> dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar tortor sit amet ex vehicula, in sollicitudin metus elementum. Phasellus efficitur metus et felis elementum, vel semper ligula tristique. Aliquam nec lectus ac lectus fermentum euismod. Vivamus augue dui, mattis id laoreet a, bibendum quis urna. Proin vitae leo ligula.</div>
+				<div class="my-auto"><h2>Nie robimy pajęczyny z sieci</h2> 
+Założyłeś dopiero firmę albo masz już istniejącą i potrzebujesz dobrej i wydajnej sieci komputerowej? U nas to nie problem. Zaprojektujemy, zainstalujemy i będziemy administrować twoją sieć. Wystarczy, że się skontaktujesz z nami telefonicznie lub przez email.
+
+
 			</div>
 		</div>	
 	</div>
@@ -53,8 +58,8 @@
 <div class="section" id="oferta">
 		<div class="row align-items-center">
 			<div class="col-sm-6" style="padding-left:0; padding-right:0;">
-				<div class="text-block my-auto"><h2>Zajmujemy się Sieciami komputerowymi</h2>
-					Prawidłowo i wydajnie funkcjonująca sieć komputerowa to żaden luksus – to warunek Państwa sukcesu. Zapewnimy Państwu dobór wszelkiego sprzętu i urządzeń niezbędnych do bezproblemowego działania firmowej sieci komputerowej.
+				<div class="text-block my-auto"><h2>Sieć komputerowa w twojej firmie</h2>
+					Prawidłowo i wydajnie funkcjonująca sieć komputerowa<br />to żaden luksus – to warunek Państwa sukcesu. Zapewnimy Państwu dobór wszelkiego sprzętu i urządzeń niezbędnych do bezproblemowego działania firmowej sieci komputerowej.
 				</div>
 			</div>
 
@@ -87,19 +92,40 @@
 <!-- #################### O nas ##################### -->
 
 <div class="section-background" id="oNas" style="background-color:#FFF;padding-bottom:50px; padding-top:50px;">
+
+<hr class="separator" style="margin-bottom:50px;margin-top:40px; border-bottom:1px solid rgba(0,0,0,0.6);"/>
 <div class="section white">
 		<div class="row align-items-center ">
 			<div class="col-sm-6">
-				<div class="col-max text-block" style="float:right; margin-bottom:40px;"><h2>Profesjonalni</h2> dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar tortor sit amet ex vehicula, in sollicitudin metus elementum. Phasellus efficitur metus et felis elementum, vel semper ligula tristique. Aliquam nec lectus ac lectus fermentum euismod. Vivamus augue dui, mattis id laoreet a, bibendum quis urna. Proin vitae leo ligula.</div>
-				<div class="col-max text-block" style="float:right;"><h2>Rzetelni</h2> dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar tortor sit amet ex vehicula, in sollicitudin metus elementum. Phasellus efficitur metus et felis elementum, vel semper ligula tristique. Aliquam nec lectus ac lectus fermentum euismod. Vivamus augue dui, mattis id laoreet a, bibendum quis urna. Proin vitae leo ligula.</div>
+				<div class="col-max text-block" style="float:right; margin-bottom:40px;"><h2>Profesjonalni</h2>
+Jesteśmy profesjonalną firmą działająca na rynku już od 25 lat. Zajmujemy się oprogramowaniem, zakładaniem sieci komputerowych oraz ich administrowaniem.
+Zapewniamy sprawną i szybką obsługę systemów informatycznych.
+</div>
 			</div>
 			<div class="col-sm-6 col-img order-first order-sm-last ">
 				<img src="<?php echo bloginfo('template_directory'); ?>/assets/images/4.png" class="img-fluid " style="float:right;"/>
 			</div>	
 		</div>
 </div>
+<hr class="separator" style="margin-bottom:50px; border-bottom:1px solid rgba(0,0,0,0.6);"/>
 </div>
 
+<div class="section-background" style="background-color:#FFF;padding-bottom:50px; padding-top:50px;">
+<div class="section white">
+	<div class="row">
+		<div class="col-md-7">
+			<img src="<?php echo bloginfo('template_directory'); ?>/assets/images/6.png" class="img-fluid" width="900" />
+		</div>
+		<div class="col-md">
+			<div class="contact-form">
+			<h2 style="margin-left:0; margin-bottom:20px;">Masz pytania? Napisz do nas.</h2>
+				<?php echo do_shortcode('[contact-form-7 id="24" title="Kontakt"]' ); ?>
+			</div>
+		</div>
+	</div>
+<hr class="separator" style="margin-bottom:50px; border-bottom:1px solid rgba(0,0,0,0.6);"/>
+</div>
+</div>
 <hr class="separator" style="margin-bottom:70px;"/>
 <!-- ################ MAPA ################ -->
 <div class="section-background" id="kontakt" style="background-color:#000">
@@ -138,7 +164,6 @@
 		</div>
 </div>
 </div>
-
 
 </div>
 <div class="container"></div>
